24b68251837e81ccddb6e50a55144b4bedb9e554
choigy1001 <easytwo10@naver.com>
2023-10-23 20:40:12 +0900

    git init

---
+++ meongcare/src/main/java/com/meongcare/MeongcareApplication.java
@@ -0,0 +1,13 @@
+package com.meongcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongcareApplication.class, args);
+	}
+
+}
---
+++ meongcare/src/test/java/com/meongcare/MeongcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meongcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
