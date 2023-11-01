8128300b3db5e889210ef1bfec0c6bf428476927
choigy1001 <easytwo10@naver.com>
2023-11-01 15:38:41 +0900

    feat: 접근 제어자 추가

--- meongcare/src/main/java/com/meongcare/domain/excreta/presentation/dto/request/DeleteExcretaRequest.java
+++ meongcare/src/main/java/com/meongcare/domain/excreta/presentation/dto/request/DeleteExcretaRequest.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class DeleteExcretaRequest {
 
     @NotNull
-    List<Long> excretaIds;
+    private List<Long> excretaIds;
 }
--- meongcare/src/main/java/com/meongcare/domain/symptom/presentation/dto/request/DeleteSymptomRequest.java
+++ meongcare/src/main/java/com/meongcare/domain/symptom/presentation/dto/request/DeleteSymptomRequest.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class DeleteSymptomRequest {
 
     @NotNull
-    List<Long> symptomIds;
+    private List<Long> symptomIds;
 }
