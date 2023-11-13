af6262f9c9f4817d180c15de36d99844102b7d3c
cheon4050 <ehdcjs159@gmail.com>
2023-11-13 19:34:24 +0900

    style: dto Swagger 스키자 적용 #25

--- meongcare/src/main/java/com/meongcare/domain/notice/presentation/dto/request/PatchNoticeRequest.java
+++ meongcare/src/main/java/com/meongcare/domain/notice/presentation/dto/request/PatchNoticeRequest.java
@@ -1,5 +1,6 @@
 package com.meongcare.domain.notice.presentation.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
@@ -8,12 +9,15 @@ import javax.validation.constraints.Size;
 @Getter
 public class PatchNoticeRequest {
 
+    @Schema(description = "공지사항 고유 ID", example = "1")
     @NotNull
     private Long noticeId;
 
+    @Schema(description = "수정할 공지사항 제목", example = "[공자] 수정된 공지 제목")
     @NotNull
     private String title;
 
+    @Schema(description = "수정할 공지사항 내용", example = "수정된 공지사항 입니다.")
     @NotNull
     @Size(max = 2000)
     private String text;
--- meongcare/src/main/java/com/meongcare/domain/notice/presentation/dto/request/SaveNoticeRequest.java
+++ meongcare/src/main/java/com/meongcare/domain/notice/presentation/dto/request/SaveNoticeRequest.java
@@ -3,6 +3,7 @@ package com.meongcare.domain.notice.presentation.dto.request;
 
 import com.meongcare.domain.notice.domain.entity.Notice;
 import com.meongcare.domain.notice.domain.entity.NoticeType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
@@ -10,13 +11,17 @@ import javax.validation.constraints.Size;
 
 @Getter
 public class SaveNoticeRequest {
+
+    @Schema(description = "공지사항 제목", example = "[공자] 공지 제목")
     @NotNull
     private String title;
 
+    @Schema(description = "공지사항 내용", example = "공지사항 입니다.")
     @NotNull
     @Size(max = 2000)
     private String text;
 
+    @Schema(description = "공지사항 타입 (event/notice)", example = "event")
     @NotNull
     private String type;
 
