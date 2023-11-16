02fe16bf8eb2be762844daab12ad0e52c8b0d623
choigy1001 <easytwo10@naver.com>
2023-11-16 11:58:23 +0900

    feat: 파일 경로 추가

--- meongcare/src/main/java/com/meongcare/infra/image/ImageDirectory.java
+++ meongcare/src/main/java/com/meongcare/infra/image/ImageDirectory.java
@@ -7,6 +7,7 @@ public enum ImageDirectory {
     EXCRETA("meongcare/excreta/"),
     SYMPTOM("meongcare/symptom/"),
     MEDICAL_RECORD("meongcare/medical-record/"),
+    FEED("meongcare/feed"),
     ;
 
     private final String baseDirectory;
