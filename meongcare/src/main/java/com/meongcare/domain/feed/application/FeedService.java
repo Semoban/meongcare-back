1a59e727d4da0cdadd063e4f5b48120a84dcb4d4
choigy1001 <easytwo10@naver.com>
2023-11-16 12:00:28 +0900

    feat: 사료 기록 조회 및 사료 조회 FeedService 구현

---
+++ meongcare/src/main/java/com/meongcare/domain/feed/application/FeedService.java
@@ -0,0 +1,85 @@
+package com.meongcare.domain.feed.application;
+
+import com.meongcare.domain.dog.domain.DogRepository;
+import com.meongcare.domain.dog.domain.entity.Dog;
+import com.meongcare.domain.feed.domain.entity.Feed;
+import com.meongcare.domain.feed.domain.entity.FeedRecord;
+import com.meongcare.domain.feed.domain.repository.FeedQueryRepository;
+import com.meongcare.domain.feed.domain.repository.FeedRecordQueryRepository;
+import com.meongcare.domain.feed.domain.repository.FeedRecordRepository;
+import com.meongcare.domain.feed.domain.repository.FeedRepository;
+import com.meongcare.domain.feed.presentation.dto.request.SaveFeedRequest;
+import com.meongcare.domain.feed.presentation.dto.response.GetFeedResponse;
+import com.meongcare.domain.feed.presentation.dto.response.GetFeedsPartResponse;
+import com.meongcare.domain.feed.presentation.dto.response.GetFeedRecordsResponse;
+import com.meongcare.domain.feed.presentation.dto.response.GetFeedsResponse;
+import com.meongcare.domain.feed.presentation.dto.response.vo.GetFeedRecordsPartVO;
+import com.meongcare.domain.feed.presentation.dto.response.vo.GetFeedRecordsVO;
+import com.meongcare.domain.feed.presentation.dto.response.vo.GetFeedsVO;
+import com.meongcare.infra.image.ImageDirectory;
+import com.meongcare.infra.image.ImageHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.meongcare.common.util.LocalDateTimeUtils.getBetweenDays;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class FeedService {
+
+    private final DogRepository dogRepository;
+    private final FeedRepository feedRepository;
+    private final FeedQueryRepository feedQueryRepository;
+    private final FeedRecordRepository feedRecordRepository;
+    private final FeedRecordQueryRepository feedRecordQueryRepository;
+    private final ImageHandler imageHandler;
+
+    @Transactional
+    public void saveFeed(SaveFeedRequest request, MultipartFile multipartFile) {
+        Dog dog = dogRepository.getById(request.getDogId());
+        String imageURL = imageHandler.uploadImage(multipartFile, ImageDirectory.FEED);
+
+        Feed feed = Feed.of(request, imageURL, dog);
+        feedRepository.save(feed);
+        //기획에 따라 추가될 수도 있는 부분
+//        feedRecordRepository.save(FeedRecord.of(feed, dog.getId()));
+    }
+
+    public GetFeedResponse getFeed(Long dogId) {
+        FeedRecord feedRecord = feedRecordQueryRepository.getFeedRecord(dogId);
+
+        return GetFeedResponse.of(
+                getBetweenDays(feedRecord.getStartDate(), LocalDateTime.now()),
+                feedRecord.getFeed()
+        );
+    }
+
+    public GetFeedsPartResponse getFeedRecordsPart(Long dogId) {
+        List<GetFeedRecordsPartVO> feedsPartVO = feedRecordQueryRepository.getFeedRecordsPartByDogId(dogId);
+        return GetFeedsPartResponse.from(feedsPartVO);
+    }
+
+    public GetFeedRecordsResponse getFeedRecords(Long dogId) {
+        List<GetFeedRecordsVO> getFeedRecordsVO = feedRecordQueryRepository.getFeedRecordsByDogId(dogId);
+        return GetFeedRecordsResponse.from(getFeedRecordsVO);
+    }
+
+    @Transactional
+    public void changeFeed(Long dogId, Long newFeedId) {
+        FeedRecord feedRecord = feedRecordQueryRepository.getFeedRecord(dogId);
+        feedRecord.updateEndDate();
+        Feed feed = feedRepository.getById(newFeedId);
+        feedRecordRepository.save(FeedRecord.of(feed, dogId));
+    }
+
+    public GetFeedsResponse getFeeds(Long dogId) {
+        List<GetFeedsVO> feedsVO = feedQueryRepository.getFeedsByDogId(dogId);
+        return GetFeedsResponse.from(feedsVO);
+    }
+}
