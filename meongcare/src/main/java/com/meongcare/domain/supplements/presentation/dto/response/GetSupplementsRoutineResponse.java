4b532528715c19f866ffa1200dde952422f9c778
cheon4050 <ehdcjs159@gmail.com>
2023-11-28 04:29:35 +0900

    fix: 영양제 루틴 섭취량 조회 방식 수정 #31

--- meongcare/src/main/java/com/meongcare/domain/supplements/presentation/dto/response/GetSupplementsRoutineResponse.java
+++ meongcare/src/main/java/com/meongcare/domain/supplements/presentation/dto/response/GetSupplementsRoutineResponse.java
@@ -32,11 +32,8 @@ public class GetSupplementsRoutineResponse {
         @NotNull
         private LocalTime intakeTime;
 
-        @Schema(description = "섭취량", example = "3")
-        private int intakeCount;
-
-        @Schema(description = "섭취 단위", example = "포")
-        private String intakeUnit;
+        @Schema(description = "섭취량", example = "3포")
+        private String intakeUnitCount;
 
         @Schema(description = "섭취 여부", example = "true")
         private boolean intakeStatus;
@@ -48,8 +45,7 @@ public class GetSupplementsRoutineResponse {
                         routine.getSupplementsRecordId(),
                         routine.getName(),
                         routine.getIntakeTime(),
-                        routine.getIntakeCount(),
-                        routine.getIntakeUnit(),
+                        createIntakeCountUnit(routine.getIntakeCount(), routine.getIntakeUnit()),
                         routine.isIntakeStatus()
                 ))
                 .collect(Collectors.toUnmodifiableList());
@@ -63,12 +59,16 @@ public class GetSupplementsRoutineResponse {
                         null,
                         routine.getName(),
                         routine.getIntakeTime(),
-                        routine.getIntakeCount(),
-                        routine.getIntakeUnit(),
+                        createIntakeCountUnit(routine.getIntakeCount(), routine.getIntakeUnit()),
                         false
                 ))
                 .collect(Collectors.toUnmodifiableList());
 
         return new GetSupplementsRoutineResponse(routines);
+    }
+
+    private static String createIntakeCountUnit(int intakeCount, String intakeUnit) {
+        return intakeCount + intakeUnit;
+
     }
 }
