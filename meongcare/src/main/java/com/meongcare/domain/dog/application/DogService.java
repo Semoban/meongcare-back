1468dea829ce735ad790dc5ee031bf2e5cd0e7ef
choigy1001 <easytwo10@naver.com>
2023-12-01 13:13:43 +0900

    feat: 메인 홈 강아지 모든 정보 조회 Service 구현

--- meongcare/src/main/java/com/meongcare/domain/dog/application/DogService.java
+++ meongcare/src/main/java/com/meongcare/domain/dog/application/DogService.java
@@ -1,5 +1,10 @@
 package com.meongcare.domain.dog.application;
 
+import com.meongcare.common.util.LocalDateTimeUtils;
+import com.meongcare.domain.dog.presentation.dto.response.GetAllRecordOfDogResponse;
+import com.meongcare.domain.excreta.domain.repository.ExcretaQueryRepository;
+import com.meongcare.domain.excreta.domain.repository.vo.GetExcretaVO;
+import com.meongcare.domain.feed.domain.repository.FeedRecordQueryRepository;
 import com.meongcare.domain.member.domain.entity.Member;
 import com.meongcare.domain.member.domain.repository.MemberRepository;
 import com.meongcare.domain.dog.domain.DogRepository;
@@ -8,7 +13,11 @@ import com.meongcare.domain.dog.presentation.dto.request.PutDogRequest;
 import com.meongcare.domain.dog.presentation.dto.request.SaveDogRequest;
 import com.meongcare.domain.dog.presentation.dto.response.GetDogResponse;
 import com.meongcare.domain.dog.presentation.dto.response.GetDogsResponse;
+import com.meongcare.domain.supplements.domain.repository.SupplementsRecordQueryRepository;
+import com.meongcare.domain.symptom.domain.repository.SymptomQueryRepository;
+import com.meongcare.domain.symptom.domain.repository.vo.GetSymptomVO;
 import com.meongcare.domain.weight.domain.entity.Weight;
+import com.meongcare.domain.weight.domain.repository.WeightQueryRepository;
 import com.meongcare.domain.weight.domain.repository.WeightRepository;
 import com.meongcare.infra.image.ImageDirectory;
 import com.meongcare.infra.image.ImageHandler;
@@ -16,6 +25,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Service
@@ -26,6 +38,11 @@ public class DogService {
     private final MemberRepository memberRepository;
     private final DogRepository dogRepository;
     private final WeightRepository weightRepository;
+    private final SymptomQueryRepository symptomQueryRepository;
+    private final WeightQueryRepository weightQueryRepository;
+    private final ExcretaQueryRepository excretaQueryRepository;
+    private final FeedRecordQueryRepository feedRecordQueryRepository;
+    private final SupplementsRecordQueryRepository supplementsRecordQueryRepository;
     private final ImageHandler imageHandler;
 
     @Transactional
@@ -66,5 +83,30 @@ public class DogService {
     @Transactional
     public void deleteDog(Long dogId) {
         dogRepository.deleteById(dogId);
+    }
+
+    public GetAllRecordOfDogResponse getDogRecord(Long dogId, LocalDateTime dateTime) {
+        List<GetSymptomVO> symptomVO = symptomQueryRepository.getSymptomByDogIdAndSelectedDate(
+                dogId,
+                LocalDateTimeUtils.createNowMidnight(dateTime),
+                LocalDateTimeUtils.createNextMidnight(dateTime)
+        );
+        List<GetExcretaVO> excretaVO = excretaQueryRepository.getByDogIdAndSelectedDate(
+                dogId,
+                LocalDateTimeUtils.createNowMidnight(dateTime),
+                LocalDateTimeUtils.createNextMidnight(dateTime)
+        );
+        double weight = weightQueryRepository.getDayWeightByDogIdAndDateTime(
+                dogId,
+                LocalDateTimeUtils.createNowMidnight(dateTime),
+                LocalDateTimeUtils.createNextMidnight(dateTime)
+        );
+        Integer recommendIntake = feedRecordQueryRepository.getFeedRecordByDogIdAndDate(dogId, dateTime);
+
+        int totalRecordCount = supplementsRecordQueryRepository.getTotalRecordCount(dogId, dateTime.toLocalDate());
+        int intakeRecordCount = supplementsRecordQueryRepository.getIntakeRecordCount(dogId, dateTime.toLocalDate());
+        return GetAllRecordOfDogResponse.of(
+                symptomVO, excretaVO, weight, recommendIntake, intakeRecordCount, totalRecordCount
+        );
     }
 }
