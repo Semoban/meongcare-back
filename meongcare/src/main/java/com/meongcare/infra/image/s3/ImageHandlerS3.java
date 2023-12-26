30d2dd66c31f1e543cb9d54b094805dcfc32afe9
choigy1001 <easytwo10@naver.com>
2023-12-26 18:05:54 +0900

    feat: s3 이미지 빈 문자열 처리

--- meongcare/src/main/java/com/meongcare/infra/image/s3/ImageHandlerS3.java
+++ meongcare/src/main/java/com/meongcare/infra/image/s3/ImageHandlerS3.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
 
@@ -43,6 +44,9 @@ public class ImageHandlerS3 implements ImageHandler {
 
     @Override
     public void deleteImage(String imageURL) {
+        if (imageURL.isEmpty()) {
+            return;
+        }
         s3Api.removeImage(bucket, imageURL);
     }
 
