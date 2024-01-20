4b60f6ce4d5b43007aeabbfd3a8576293a0528b0
0sunset0 <chi6465618@naver.com>
2024-01-20 14:37:23 +0900

    feat: 검증 에러 핸들러 (#92)

--- meongcare/src/main/java/com/meongcare/common/error/GlobalExceptionHandler.java
+++ meongcare/src/main/java/com/meongcare/common/error/GlobalExceptionHandler.java
@@ -3,24 +3,26 @@ package com.meongcare.common.error;
 import com.meongcare.common.error.exception.EntityNotFoundException;
 import com.meongcare.common.error.exception.FailedFileUploadException;
 import com.meongcare.common.error.exception.InvalidTokenException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    protected ResponseEntity<ErrorResponse> runtimeException(RuntimeException e) {
-        e.printStackTrace();
-        final ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
-    }
 
     @ExceptionHandler(InvalidTokenException.class)
     protected ResponseEntity<ErrorResponse> invalidTokenException(InvalidTokenException e) {
         e.printStackTrace();
+        log.warn("{} - {}", e.getClass().getSimpleName(), e.getMessage());
         final ErrorResponse errorResponse = new ErrorResponse(e.getHttpStatus(), e.getMessage());
         return ResponseEntity.status(e.getHttpStatus()).body(errorResponse);
     }
@@ -28,6 +30,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     protected ResponseEntity<ErrorResponse> entityNotFoundException(EntityNotFoundException e) {
         e.printStackTrace();
+        log.warn("{} - {}", e.getClass().getSimpleName(), e.getMessage());
         final ErrorResponse errorResponse = new ErrorResponse(e.getHttpStatus(), e.getMessage());
         return ResponseEntity.status(e.getHttpStatus()).body(errorResponse);
     }
@@ -35,7 +38,37 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(FailedFileUploadException.class)
     protected ResponseEntity<ErrorResponse> failedFileUploadException(FailedFileUploadException e) {
         e.printStackTrace();
+        log.warn("{} - {}", e.getClass().getSimpleName(), e.getMessage());
         final ErrorResponse errorResponse = new ErrorResponse(e.getHttpStatus(), e.getMessage());
         return ResponseEntity.status(e.getHttpStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<ErrorResponse> runtimeException(RuntimeException e) {
+        e.printStackTrace();
+        log.warn("{} - {}", e.getClass().getSimpleName(), e.getMessage());
+        final ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> exception(Exception e) {
+        e.printStackTrace();
+        log.warn("{} - {}", e.getClass().getSimpleName(), e.getMessage());
+        final ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    //validation 에러
+    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> bindException(BindingResult bindingResult) {
+        StringBuilder reason = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            String errorMessage = fieldError.getField() + " : " + fieldError.getDefaultMessage();
+            reason.append(errorMessage).append(", ");
+        }
+        log.warn("ValidationException - {}", reason);
+        final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, String.valueOf(reason));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
