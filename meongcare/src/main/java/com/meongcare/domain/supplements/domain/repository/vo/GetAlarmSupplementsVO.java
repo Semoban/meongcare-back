fed0fcef2502f7d2708c6220f19f05b1513c83af
Keun young <easytwo10@naver.com>
2024-02-04 20:40:06 +0900

    알림 이벤트 발행 및 비동기 처리, 알림 전송 실패 시 DB에 저장 (#127)
    * 알림 스케줄러, 서비스 로직 분리
    
    * 스케줄러와 서비스 로직 분리, 알림 이벤트 발행하도록 구현
    
    * 알림 비동기 처리, 실패 시 DB에 기록 남기도록 추가(

--- meongcare/src/main/java/com/meongcare/domain/supplements/domain/repository/vo/GetAlarmSupplementsVO.java
+++ meongcare/src/main/java/com/meongcare/domain/supplements/domain/repository/vo/GetAlarmSupplementsVO.java
@@ -8,11 +8,15 @@ public class GetAlarmSupplementsVO {
     private String fcmToken;
     private String dogName;
     private String supplementsName;
+    private Long memberId;
+    private Long dogId;
 
     @QueryProjection
-    public GetAlarmSupplementsVO(String fcmToken, String dogName, String supplementsName) {
+    public GetAlarmSupplementsVO(String fcmToken, String dogName, String supplementsName, Long memberId, Long dogId) {
         this.fcmToken = fcmToken;
         this.dogName = dogName;
         this.supplementsName = supplementsName;
+        this.memberId = memberId;
+        this.dogId = dogId;
     }
 }
