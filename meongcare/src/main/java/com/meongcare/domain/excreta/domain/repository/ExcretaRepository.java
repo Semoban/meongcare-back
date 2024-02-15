2b35ba89da49cdfe387c268ab3bd1a7c50e388aa
choigy1001 <easytwo10@naver.com>
2024-02-15 23:41:04 +0900

    삭제된 컬럼 조회하지 않도록 변경

--- meongcare/src/main/java/com/meongcare/domain/excreta/domain/repository/ExcretaRepository.java
+++ meongcare/src/main/java/com/meongcare/domain/excreta/domain/repository/ExcretaRepository.java
@@ -5,10 +5,14 @@ import com.meongcare.common.error.exception.clientError.EntityNotFoundException;
 import com.meongcare.domain.excreta.domain.entity.Excreta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExcretaRepository extends JpaRepository<Excreta, Long> {
 
     default Excreta getById(Long id) {
-        return this.findById(id)
+        return this.findByIdAndDeletedFalse(id)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.EXCRETA_ENTITY_NOT_FOUND));
     }
+
+    Optional<Excreta> findByIdAndDeletedFalse(Long id);
 }
