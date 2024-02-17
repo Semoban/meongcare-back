d5c2d62229cbe9981b0a9d20496b2137d791d17c
choigy1001 <easytwo10@naver.com>
2024-02-17 18:08:41 +0900

    코드 리팩터링

--- meongcare/src/main/java/com/meongcare/domain/weight/presentation/dto/response/GetMonthWeightResponse.java
+++ meongcare/src/main/java/com/meongcare/domain/weight/presentation/dto/response/GetMonthWeightResponse.java
@@ -1,13 +1,9 @@
 package com.meongcare.domain.weight.presentation.dto.response;
 
-import com.meongcare.domain.weight.domain.repository.vo.GetMonthWeightVO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
@@ -18,18 +14,7 @@ public class GetMonthWeightResponse {
     @Schema(description = "이번달 몸무게", example = "36.3")
     private double thisMonthWeight;
 
-    public static GetMonthWeightResponse of(List<GetMonthWeightVO> weightVO, LocalDate date) {
-        double lastMonthWeight = 0;
-        double thisMonthWeight = 0;
-
-        for (GetMonthWeightVO vo : weightVO) {
-            if (vo.getMonth() == date.getMonthValue()) {
-                thisMonthWeight = vo.getWeight();
-            }
-            if (vo.getMonth() == date.minusMonths(1).getMonthValue()) {
-                lastMonthWeight = vo.getWeight();
-            }
-        }
+    public static GetMonthWeightResponse of(double lastMonthWeight, double thisMonthWeight) {
         return new GetMonthWeightResponse(lastMonthWeight, thisMonthWeight);
     }
 }
