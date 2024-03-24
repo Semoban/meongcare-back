882efa5176ba5e8d117c0c990265a118ca5d1835
choigy1001 <easytwo10@naver.com>
2024-03-24 20:28:50 +0900

    대소변 이미지 관련 API 수정

--- meongcare/src/main/java/com/meongcare/domain/excreta/presentation/dto/request/PatchExcretaRequest.java
+++ meongcare/src/main/java/com/meongcare/domain/excreta/presentation/dto/request/PatchExcretaRequest.java
@@ -1,6 +1,7 @@
 package com.meongcare.domain.excreta.presentation.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
@@ -20,4 +21,7 @@ public class PatchExcretaRequest {
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = COMMON_PATTERN, timezone = "Asia/Seoul")
     private LocalDateTime dateTime;
+
+    @Schema(description = "이미지 S3 링크")
+    private String imageURL;
 }
--- meongcare/src/main/java/com/meongcare/domain/excreta/presentation/dto/request/SaveExcretaRequest.java
+++ meongcare/src/main/java/com/meongcare/domain/excreta/presentation/dto/request/SaveExcretaRequest.java
@@ -1,6 +1,7 @@
 package com.meongcare.domain.excreta.presentation.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
@@ -12,12 +13,17 @@ import static com.meongcare.common.DateTimePattern.COMMON_PATTERN;
 public class SaveExcretaRequest {
 
     @NotNull
+    @Schema(description = "강아지 ID")
     private Long dogId;
 
     @NotNull
+    @Schema(description = "대소변")
     private String excreta;
 
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = COMMON_PATTERN, timezone = "Asia/Seoul")
     private LocalDateTime dateTime;
+
+    @Schema
+    private String imageURL;
 }
