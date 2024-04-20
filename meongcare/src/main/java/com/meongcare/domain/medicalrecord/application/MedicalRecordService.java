2e00b6d0244db3f552b483da7d99b8a631d18d88
choigy1001 <easytwo10@naver.com>
2024-04-20 22:53:48 +0900

    medicalRecord preSignedURL 적용

--- meongcare/src/main/java/com/meongcare/domain/medicalrecord/application/MedicalRecordService.java
+++ meongcare/src/main/java/com/meongcare/domain/medicalrecord/application/MedicalRecordService.java
@@ -32,19 +32,15 @@ public class MedicalRecordService {
     private final MedicalRecordQueryRepository medicalRecordQueryRepository;
 
     @Transactional
-    public void save(MultipartFile multipartFile, SaveMedicalRecordRequest saveMedicalRecordRequest) {
+    public void save(SaveMedicalRecordRequest saveMedicalRecordRequest) {
         Dog dog = dogRepository.getDog(saveMedicalRecordRequest.getDogId());
-        String imageURL = imageHandler.uploadImage(multipartFile, ImageDirectory.MEDICAL_RECORD);
-
-        medicalRecordRepository.save(saveMedicalRecordRequest.toEntity(dog, imageURL));
+        medicalRecordRepository.save(saveMedicalRecordRequest.toEntity(dog));
     }
 
     @Transactional
-    public void update(MultipartFile multipartFile, PutMedicalRecordRequest putMedicalRecordRequest) {
+    public void update(PutMedicalRecordRequest putMedicalRecordRequest) {
         MedicalRecord medicalRecord = medicalRecordRepository.getById(putMedicalRecordRequest.getMedicalRecordId());
-        String imageURL = imageHandler.uploadImage(multipartFile, ImageDirectory.MEDICAL_RECORD);
-
-        medicalRecord.updateMedicalRecord(putMedicalRecordRequest, imageURL);
+        medicalRecord.updateMedicalRecord(putMedicalRecordRequest);
     }
 
     @Transactional
