d0f26ca516d045d3180451092d79d7ee4e91c713
0sunset0 <chi6465618@naver.com>
2024-05-05 23:59:50 +0900

    refactor: import문 정리

--- meongcare/src/main/java/com/meongcare/domain/dog/application/DogScheduler.java
+++ meongcare/src/main/java/com/meongcare/domain/dog/application/DogScheduler.java
@@ -3,9 +3,7 @@ package com.meongcare.domain.dog.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
 
 
 @RequiredArgsConstructor
